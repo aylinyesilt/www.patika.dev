d26e450ff6dbcf8a57839439921e2d6bdef97868
aylinyesilt <108435979+aylinyesilt@users.noreply.github.com>
2022-06-30 22:08:53 +0300

    Create ODEV2.sql

---
+++ ODEV2.sql
@@ -0,0 +1,3 @@
+SELECT * FROM FILM WHERE replacement_cost between 12.99 and 16.99 ;
+Select actor from film where first_name IN ('Penelope' 'Ed' 'Nick') ;
+select * from film where rental_rate IN (0.99, 2.99, 4.99) AND replacemen_cost IN (12,99, 15.99, 28.99) ;
